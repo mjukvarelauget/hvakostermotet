46e6fba5ea0dcf7e1d29dd92d036619bedc98fe7
Grimmbol <krogstie.oystein@gmail.com>
2021-09-26 13:28:25 +0200

    add controller script to read from participant list to objects

--- scripts/participant-controller.js
+++ scripts/participant-controller.js
@@ -1,9 +1,67 @@
 import {updateHourCost} from "./projection-controller.js";
 
-updateHourCost(6200);
+// Bookkeeping
+let numParticipants = 0;
+let totalHourCost = 0;
+
+// DOM object handles
+let participantListDOM;
+
+// Internal state
+let participants = [];
+class Participant {
+  constructor(name, position, pay) {
+    this.name = name;
+    this.position = position;
+    this.pay = pay;
+  }
+}
+
+
+const setupHandles = () => {
+  participantListDOM = document.querySelectorAll(".participant-list-wrapper li");
+}
+
+const removeWhitespace = (string) => {
+  const whitespace = ['\n', '\t', ' '];
+  for(let char of whitespace) {
+    string = string.replaceAll(char, '');
+  }
+  return string;
+}
+
+// Translate from DOM list element into participant object
+const readParticipants = () => {
+  participants = [];
+  for(let participantListElem of participantListDOM) {
+    let name =
+      removeWhitespace(participantListElem.querySelector(".name").textContent);
+    let position =
+      removeWhitespace(participantListElem.querySelector(".position").textContent);
+    let pay =
+      removeWhitespace(participantListElem.querySelector(".pay").textContent);
+
+    let curParticipant = new Participant(name, position, parseInt(pay, 10));
+    participants.push(curParticipant);
+  }
+  numParticipants = participants.length;
+}
 
 // Scan through the list of participants, get payment, add it up
 const getTotalHourPay = ()  => {
-  
+  totalHourCost = 0;
+  for(let participant of participants) {
+    totalHourCost += participant.pay;
+  }
 }
+
+
+const init = (e) => {
+  setupHandles();
+  readParticipants();
+  getTotalHourPay();
+  updateHourCost(totalHourCost);
+}
+
+window.addEventListener('load', init);
 
